3b6817c57b229a218005243db5b2917cfe87a6f8
du-masa <m_ueda@wanaps.jp>
2019-07-01 23:09:29 +0900

    2

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const addListItem = () => {
   const $list = document.getElementById("list");
   const $li = document.createElement("li");
-  $li.innerHTML = `list item ${Math.floor(Math.random() * 20)}`;
+  const $p = document.createElement("p");
+  const $img = document.createElement("img");
+  $p.innerHTML = `list item ${Math.floor(Math.random() * 20)}`;
+  $img.src = "https://unsplash.it/200?random"
+  $li.appendChild($img);
+  $li.appendChild($p);
   $list.appendChild($li);
-}
-
-const handleScroll = () => {
-  console.log("handleScroll");
 }
 
 const hardLoop = () => {
@@ -18,4 +19,32 @@ const hardLoop = () => {
 document.getElementById("addElement").addEventListener("click", addListItem);
 document.getElementById("hardLoop").addEventListener("click", hardLoop);
 
-window.addEventListener("scroll", handleScroll);
+window.addEventListener("scroll", () => {
+  console.log("affa");
+});
+
+var x = [];
+
+function grow() {
+  x.push(new Array(1000000).join('x'));
+}
+
+document.getElementById('grow').addEventListener('click', grow);
+
+var detachedNodes;
+
+function create() {
+  var ul = document.createElement('ul');
+  for (var i = 0; i < 10; i++) {
+    var li = document.createElement('li');
+    ul.appendChild(li);
+  }
+  detachedTree = ul;
+}
+
+function remove() {
+  detachedTree = null;
+}
+
+document.getElementById('create').addEventListener('click', create);
+document.getElementById('remove').addEventListener('click', remove);
